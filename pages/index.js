fc92fba43d4ed0aaed2265fbc1f4c156d732dba1
软盘君 <68799904+ifloppy@users.noreply.github.com>
2023-02-01 07:46:54 +0000

    加群和Discord服务器按钮

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,17 @@
-import { Container, Spacer, Grid, Button, Text, Card } from '@nextui-org/react'
+
+import React from "react";
+import { Container, Spacer, Grid, Button, Text, Card, Modal, useModal } from '@nextui-org/react'
 
 export default function Home() {
+  const [visible, setVisible] = React.useState(false);
+  const joinQQGroupHandler = () => {setVisible(true);window.open('https://jq.qq.com/?_wv=1027&k=Rh5tyeB1', '_blank');};
+  const joinDiscordHandler = () => {window.open('https://discord.gg/Jr3G4P7gZ6', '_blank');};
+
+  const closeHandler = () => {
+    setVisible(false);
+    
+  };
+
   return (
     <Container>
       <Grid.Container gap={-1} justify="center">
@@ -41,12 +52,37 @@ export default function Home() {
       <Spacer y={1}></Spacer>
       <Grid.Container gap={2} justify="center">
         <Grid>
-          <Button size='xl'>加入QQ群</Button>
+          <Button size='xl' auto shadow onPress={joinQQGroupHandler}>加入QQ群</Button>
         </Grid>
         <Grid>
-          <Button size='xl'>加入Discord</Button>
+          <Button size='xl' auto shadow onPress={joinDiscordHandler}>加入Discord</Button>
         </Grid>
       </Grid.Container>
+
+
+      <Modal
+        closeButton
+        aria-labelledby="modal-title"
+        open={visible}
+        onClose={closeHandler}
+      >
+        <Modal.Header>
+        <Text id="modal-title" size={18}>
+            加入QQ群
+          </Text>
+        </Modal.Header>
+        <Modal.Body>
+          <Text id="modal-description">
+            若无法通过链接加入QQ群，请复制群号并搜索加群：953762913
+          </Text>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button auto onPress={() => setVisible(false)}>
+            确认
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
 
 
       <Grid.Container justify="center">
